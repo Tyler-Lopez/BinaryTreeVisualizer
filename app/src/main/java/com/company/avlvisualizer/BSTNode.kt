82b2f6ec453d3592e803fbaacde1a1e776e0f8df
BunsenBurnsRust <77797048+BunsenBurnsRust@users.noreply.github.com>
2021-11-22 21:24:39 -0500

    Node work

--- app/src/main/java/com/company/avlvisualizer/BSTNode.kt
+++ app/src/main/java/com/company/avlvisualizer/BSTNode.kt
@@ -1,18 +1,42 @@
 package com.company.avlvisualizer
 
 
-typealias Visitor<T> = (T) -> Unit
+typealias Visitor = (Int) -> Unit
 
-class BSTNode<T>(val value: T) {
-    var leftChild: BSTNode<T>? = null
-    var rightChild: BSTNode<T>? = null
 
-    fun height(node: BSTNode<T>? = this): Int {
+class BSTNode(val value: Int) {
+    var leftChild: BSTNode? = null
+    var rightChild: BSTNode? = null
+
+    fun height(node: BSTNode? = this): Int {
         return node?.let {
             1 + maxOf(
                     height(node.leftChild),
                     height(node.rightChild)
             )
         } ?: -1
+    }
+
+
+    fun traverseInOrder(visit: Visitor) {
+        leftChild?.traverseInOrder(visit) // If left child is NOT null traverse to it
+        visit(value) // Callback to visitor
+        rightChild?.traverseInOrder(visit)
+    }
+
+    override fun toString() = diagram(this)
+
+    private fun diagram(node: BSTNode?,
+                        top: String = "",
+                        root: String = "",
+                        bottom: String = ""): String {
+        return node?.let {
+            if (node.leftChild == null && node.rightChild == null) {
+                "$root${node.value}\n"
+            } else {
+                diagram(node.rightChild, "$top ", "$top┌──", "$top│ ") +
+                        root + "${node.value}\n" + diagram(node.leftChild, "$bottom│ ", "$bottom└──", "$bottom ")
+            }
+        } ?: "${root}null\n"
     }
 }
--- app/src/main/java/com/company/avlvisualizer/BSTTree.java
+++
@@ -1,4 +0,0 @@
-package com.company.avlvisualizer;
-
-public class BSTTree {
-}
